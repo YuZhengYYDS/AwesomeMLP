58bd913c8da062c6189ddff0d2e766689288a088
YuZhengYYDS <zheng.yu3@mail.mcgill.ca>
2024-09-18 15:46:05 -0400

    Add functionx

--- fig/view_mnist.py
+++ fig/view_mnist.py
@@ -18,4 +18,24 @@ import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 
-
+def main():
+    training_set, validation_set, test_set = mnist_loader.load_data()
+    images = get_images(training_set)
+    
+ 
+def get_images(training_set):
+    """ Return a list containing the images from the MNIST data
+    set. Each image is represented as a 2-d numpy array."""
+    flattened_images = training_set[0]
+    return [np.reshape(f, (-1, 28)) for f in flattened_images]
+ 
+    
+def plot_mnist_digit(image):
+    """ Plot a single MNIST image."""
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+    ax.matshow(image, cmap = matplotlib.cm.binary)
+    plt.xticks(np.array([]))
+    plt.yticks(np.array([]))
+    plt.show()
+    
